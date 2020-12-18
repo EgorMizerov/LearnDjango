9a1357672e465d6bdbfeb39bd071b8711c6bf30d
Egor Mizerov <mizerov-2003@mail.ru>
2020-12-18 12:51:42 +0500

    adв templating.html

--- views.py
+++ views.py
@@ -1,7 +1,15 @@
 # Создание обработчика
 # ====================
 from django.http import HttpResponse
+from django.shortcuts import render
+
 
 def handler(request):
     context = "Hello, World!"
     return HttpResponse(context)
+
+
+def index(request):
+    context = "Hello, World!"
+    # Возвращаем html страницу index
+    return render(request, 'path/index.html', {'text': context})
