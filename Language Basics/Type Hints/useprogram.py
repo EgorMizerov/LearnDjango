1527ff7f611fe7c917ba2e9725f4ace360f3396f
EgorMizerov <mizerov-2003@mail.ru>
2021-02-27 01:51:27 +0500

    Добавил линтер

--- Language Basics/Type Hints/useprogram.py
+++ Language Basics/Type Hints/useprogram.py
@@ -1,6 +1,4 @@
 from program import run
 
-result = run("Egor")
-
 # Если указать неверные аттрибуты или ожидаемое значение, то будет высвечиваться ошибка
-result2: int = run(25)
+result = run("Egor")
